905c780ff248f2833154f639342c5b8fb5c0f72f
changliu2 <lc92617@163.com>
2020-03-02 18:20:01 +0800

    miniapp test

---
+++ src/main/java/com/github/binarywang/demo/wx/miniapp/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.github.binarywang.demo.wx.miniapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("getUser")
+    public Map<String, Object> getUser(){
+        System.out.println("微信小程序正在调用。。。");
+        Map<String, Object> map = new HashMap<String, Object>();
+        List<String> list = new ArrayList<String>();
+        list.add("zhangsan");
+        list.add("lisi");
+        list.add("wanger");
+        list.add("mazi");
+        map.put("list",list);
+        System.out.println("微信小程序调用完成。。。");
+        return map;
+    }
+}
